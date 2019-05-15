7d91b1d0a2c569db160e701a671a5695a5d0fbc7
CSharperMantle <2649266980@qq.com>
2019-05-15 14:54:28 +0800

    add api v1.2

---
+++ src/api/v1.2/upload.php
@@ -0,0 +1,16 @@
+<?php
+    /*
+    * Copyright (c) 2018 Ningbo Foreign Language School
+    * This part of program should be delivered with the whole project.
+    * Partly use is not allowed.
+    * Licensed under GPL-v3 Agreement
+    */
+    require '..\\..\\assets\\php\\shared_db.php';
+    require '..\\..\\assets\\php\\shared_util.php';
+    require '..\\..\\assets\\php\\shared_sql.php';
+    require '..\\..\\assets\\php\\shared_json.php';
+    require '..\\..\\assets\\php\\shared_const.php';
+
+    $raw_content = file_get_contents('php://input');
+    var_dump(json_decode($, true));
+?>+
